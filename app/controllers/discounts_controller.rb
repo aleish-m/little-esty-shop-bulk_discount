c5e153341189cd8a729c4724539a8b4462cb2a92
Aleisha Mork <aleisha.mork@gmail.com>
2022-09-23 00:06:11 -0600

    feat: Add each discount that merchant has to the discount index page. Adjust site footer to look more like a footer

--- app/controllers/discounts_controller.rb
+++ app/controllers/discounts_controller.rb
@@ -1,5 +1,6 @@
 class DiscountsController < ApplicationController 
   def index
+    @discounts = Discount.where(merchant_id: params[:merchant_id])
     # require "pry"; binding.pry
   end
 end
--- spec/features/merchants/discounts/index_spec.rb
+++ spec/features/merchants/discounts/index_spec.rb
@@ -23,17 +23,18 @@ RSpec.describe 'Merchant Bulk Discounts Index' do
     describe 'I am taken to my bulk discounts index page' do
       it 'I see all of my bulk discounts including their percentage discount and quantity thresholds' do
         visit merchant_discounts_path(@merchant_1)
+        save_and_open_page
         within("#merchant-#{@merchant_1.id}-discounts") do
           within("#discount-#{@discount_1.id}") do
-            expect(page).to have_content("Discount Percentage: #{@discount_1.discount.round(2)}")
-            expect(page).to have_content("Discount Minumum Quantity: #{@discount_1.threshold}")
+            expect(page).to have_content("Discount: #{(@discount_1.discount.round(2))*100}%")
+            expect(page).to have_content("Minumum Order Quantity: #{@discount_1.threshold}")
           end
           within("#discount-#{@discount_2.id}") do
-            expect(page).to have_content("Discount Percentage: #{@discount_2.discount.round(2)}")
-            expect(page).to have_content("Discount Minumum Quantity: #{@discount_2.threshold}")
+            expect(page).to have_content("Discount: #{(@discount_2.discount.round(2))*100}%")
+            expect(page).to have_content("Minumum Order Quantity: #{@discount_2.threshold}")
           end
         end
-        expect(page).to_not have_content("Discount Percentage: #{@discount_3.discount.round(2)}")
+        expect(page).to_not have_content("Discount: #{@discount_3.discount.round(2)*100}%")
       end
       it 'Each bulk discount listed includes a link to its show page'
     end
