51bf008eb8e6729f52559d996036ae5f3ca91f30
Aleisha Mork <aleisha.mork@gmail.com>
2022-09-23 14:34:54 -0600

    test: add test for form on new discount page

--- spec/features/merchants/discounts/create_spec.rb
+++ spec/features/merchants/discounts/create_spec.rb
@@ -22,10 +22,18 @@ RSpec.describe 'Merchant Bulk Discounts Create' do
 
     describe 'I am taken to a new page to add a new bulk discount' do
       it 'I fill in the form with valid data I am redirected back to the bulk discount index' do
-        visit merchant_discounts_path(@merchant_1)
+        visit new_merchant_discount_path(@merchant_1)
+
+        fill_in "Discount Percentage:", with: "0.25"
+        fill_in "Minimum Order Quantity:", with: "10"
+        click_button "Create Discount"
+
+        expect(current_path).to eq(merchant_discounts_path(@merchant_1))
       end
 
-      it 'I see my new bulk discount listed'
+      it 'I see my new bulk discount listed' do
+        visit merchant_discounts_path(@merchant_1)
+      end
     end
   end
 end
