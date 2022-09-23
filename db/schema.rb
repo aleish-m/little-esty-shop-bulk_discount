173f00dfc355765dded3acd0b265c8e10d8c522b
Aleisha Mork <aleisha.mork@gmail.com>
2022-09-22 21:31:26 -0600

    add discounts table"

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_13_034244) do
+ActiveRecord::Schema.define(version: 2022_09_23_033004) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 2022_09_13_034244) do
   create_table "customers", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "discounts", force: :cascade do |t|
+    t.float "discount"
+    t.integer "threshold"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
