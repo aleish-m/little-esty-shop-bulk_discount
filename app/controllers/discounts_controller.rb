47a22b9f796cb45174f0556be8a9426a33940b20
Aleisha Mork <aleisha.mork@gmail.com>
2022-09-24 15:17:43 -0600

    feat: Add form to update existing discount info on discount edit page.

--- app/controllers/discounts_controller.rb
+++ app/controllers/discounts_controller.rb
@@ -6,6 +6,7 @@ class DiscountsController < ApplicationController
 
   def show
     @discount = Discount.find(params[:id])
+    @merchant = Merchant.find(params[:merchant_id])
   end
 
   def new
@@ -31,6 +32,20 @@ class DiscountsController < ApplicationController
     merchant = Merchant.find(params[:merchant_id])
     redirect_to(merchant_discounts_path(merchant))
   end
+
+  def edit 
+    @discount = Discount.find(params[:id])
+    @merchant = Merchant.find(params[:merchant_id])
+  end
+
+  def update
+    discount = Discount.find(params[:id])
+    merchant = Merchant.find(params[:merchant_id])
+    discount.update(discount_params)
+    redirect_to(merchant_discount_path(merchant, discount))
+  end
+
+
 
   private 
   def discount_params
