8536ad2366a2136cb1b0abd7d9c92c8b3c6db91a
Aleisha Mork <aleisha.mork@gmail.com>
2022-09-26 14:05:49 -0600

    show discounted revenue on admin invoice show page.

--- spec/features/merchants/invoices/show_spec.rb
+++ spec/features/merchants/invoices/show_spec.rb
@@ -16,15 +16,6 @@ RSpec.describe 'Merchant Invoice Show Page' do
 
   end
 
-  # As a merchant
-  # When I visit my merchant's invoice show page(/merchants/merchant_id/invoices/invoice_id)
-  # Then I see information related to that invoice including:
-
-  # Invoice id
-  # Invoice status
-  # Invoice created_at date in the format "Monday, July 18, 2019"
-  # Customer first and last name
-
   describe 'User Story 15 - When I visit my merchants invoice show page' do
     it 'Then I see information related to that invoice' do
       visit merchant_invoice_path(@merchant_1, @invoice_1)
@@ -55,16 +46,6 @@ RSpec.describe 'Merchant Invoice Show Page' do
     end
   end
 
-  # As a merchant
-  # When I visit my merchant invoice show page
-  # Then I see all of my items on the invoice including:
-
-  # Item name
-  # The quantity of the item ordered
-  # The price the Item sold for
-  # The Invoice Item status
-  # And I do not see any information related to Items for other merchants
-
   describe 'User Story 16 - When I visit my merchant invoice show page' do
     it 'Then I see all of my items on the invoice' do
       visit merchant_invoice_path(@merchant_1, @invoice_1)
@@ -90,17 +71,6 @@ RSpec.describe 'Merchant Invoice Show Page' do
       end
     end
   end
-
-  # As a merchant
-  # When I visit my merchant invoice show page
-  # I see that each invoice item status is a select field
-  # And I see that the invoice item's current status is selected
-  # When I click this select field,
-  # Then I can select a new status for the Item,
-  # And next to the select field I see a button to "Update Item Status"
-  # When I click this button
-  # I am taken back to the merchant invoice show page
-  # And I see that my Item's status has now been updated
 
   describe 'User Story 18 When I visit my merchant invoice show page' do
     it 'I see that each invoice item status is a select field' do
@@ -141,10 +111,6 @@ RSpec.describe 'Merchant Invoice Show Page' do
       end
     end
   end
-
-  # As a merchant
-  # When I visit my merchant invoice show page
-  # Then I see the total revenue that will be generated from all of my items on the invoice
 
   describe 'User Story 17 - When I visit my merchant invoice show page' do
     before :each do
@@ -283,6 +249,5 @@ RSpec.describe 'Merchant Invoice Show Page' do
 
       expect(current_path).to eq(merchant_discount_path(@merchant_1, @discount_2))
     end
-
   end
 end
